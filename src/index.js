097f268fb047630c7aa39dcaff9721e4d5ef161d
Monica <monica@DESKTOP-A3SK9UM.localdomain>
2023-12-29 08:21:36 -0800

    done with assignements

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  document.querySelector('form').addEventListener('submit', (e) => {
+   e.preventDefault()
+   myToDos(e.target.new_task_description.value);
+  })
+
+   
 });
+function myToDos(todo){
+  let list = document.createElement('li')
+  let btn = document.createElement('button')
+  btn.addEventListener('click', handleDelete)
+  btn.textContent = 'X'
+  list.textContent = `${todo}`
+  list.appendChild(btn)
+  console.log(list)
+  document.querySelector('#tasks').appendChild(list)
+
+}
+
+function handleDelete(e){
+  e.target.parentNode.remove()
+}
